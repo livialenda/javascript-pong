771193bf5edd36beef00470b3fbe5a4cf6e5255e
livialenda <168776379+livialenda@users.noreply.github.com>
2024-06-27 17:42:10 -0300

    cria bolinha

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,8 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -7,4 +12,11 @@ function draw(){
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
     background (175,238,238);
+    //chamando a função
+    criarBolinha(xBolinha, yBolinha, tamBolinha);
+}
+
+//função bolinha
+function criaBolinha(xBolinha,yBolinha, tamBolinha){
+    circle (xBolinha, yBolinha, tamBolinha);
 }
