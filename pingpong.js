edd7c8918325e4481154e6aa42bd0db1f64fb88c
livialenda <168776379+livialenda@users.noreply.github.com>
2024-08-01 13:43:01 -0300

    Add files via upload
    //aqui eu vou criar a bolinha
    let xBolinha = 300;
    let yBolinha = 200;
    let tamBolinha = 25;
    
    //config velocidade bolinha
    let xvelocidadeBolinha = 6;
    let yvelocidadeBolinha = 6;
    
    function setup(){
        //aqui vou criar minha "mesa"
        createCanvas(600,400);
    }
    
    function draw(){
        //função responsável pelo "desenho e animação da mesa"
        //aqui vou por a cor da "mesa"
        //A cor da mesa é em rgb
        background(128,128,0); 
        //chamando a função cria bolinha para criar a bolinha
        criaBolinha(xBolinha, yBolinha, tamBolinha);
        //chamando a função mov bolinha
        moveBolinha();
        //chamando a borda
        Borda();
    }
    
    //função bolinha
    function criaBolinha(xBolinha, yBolinha,tamBolinha){
        circle (xBolinha,yBolinha,tamBolinha);
    }
    
    //função move bolinha
    function moveBolinha(){
        xBolinha = xvelocidadeBolinha + xBolinha;
        yBolinha = yvelocidadeBolinha + yBolinha;
    }
    
    function Borda(){
        if (xBolinha > width || xBolinha < 0){
            xvelocidadeBolinha *= -1;
        }
        if (yBolinha > height || yBolinha < 0){
            yvelocidadeBolinha *= -1;
        }
    }

--- pingpong.js
+++ pingpong.js
@@ -3,20 +3,44 @@ let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
 
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
 function draw(){
+    //função responsável pelo "desenho e animação da mesa"
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
-    background (175,238,238);
-    //chamando a função
-    criarBolinha(xBolinha, yBolinha, tamBolinha);
+    background(128,128,0); 
+    //chamando a função cria bolinha para criar a bolinha
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    //chamando a função mov bolinha
+    moveBolinha();
+    //chamando a borda
+    Borda();
 }
 
 //função bolinha
-function criaBolinha(xBolinha,yBolinha, tamBolinha){
-    circle (xBolinha, yBolinha, tamBolinha);
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
+    }
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
+    }
 }
